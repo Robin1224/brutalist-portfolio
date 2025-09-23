392148baf41c1a0f611caab5bd0943292ebd7407
Robin <robin.django@gmail.com>
2025-09-23 15:25:33 +0200

    Added vercel analytics

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+  import { injectAnalytics } from "@vercel/analytics/sveltekit";
+
+  injectAnalytics();
+
+  let { children } = $props();
+</script>
+
+{@render children()}
+
+
+<style>
+</style>
